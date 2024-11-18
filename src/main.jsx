328d75e883f5a06240accfae09a1330cf69953cc
Harald Binkle <harald.binkle@xebia.com>
2024-11-18 17:13:50 +0100

    added xebia logo and valid default task

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
 // eslint-disable-next-line react-refresh/only-export-components
 const DATA = [
-  { id: "todo-0", name: "Eat", completed: true },
-  { id: "todo-1", name: "Sleep", completed: false },
-  { id: "todo-2", name: "Repeat", completed: false },
+  {
+    id: "todo-iYhueLHTq-6wprHhsXYF6",
+    name: "test",
+    time: "2024-11-18T16:12:44.160Z",
+    location: {
+      latitude: 49.6370557,
+      longitude: 6.9014314,
+    },
+    completed: false,
+  },
 ];
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App tasks={DATA} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
